e02314c5525593434eca267ad2107209d86dca2f
dev <dev@schepurs-Mac-Pro.local>
2021-01-26 12:46:08 +0300

    ok

--- make_env_macos.py
+++ make_env_macos.py
@@ -3,5 +3,5 @@ import shutil
 os.system("python3 -m venv env-macos")
 os.system("./env-macos/bin/pip install -r requirements.txt")
 shutil.copyfile("src/build/fix/macos/(pyinstaller_fix)_hook-_tkinter.py", "env-macos/lib/python3.7/site-packages/PyInstaller/hooks/hook-_tkinter.py")
-shutil.copyfile("src/build/fix/macos/(pyinstaller_fix)_hook-_tkinter.py", "env-macos/lib/python3.7/site-packages/PyInstaller/hooks/hook-_tkinter.py")
+shutil.copyfile("src/build/fix/macos/(pyinstaller_build_fix)_osx.py", "env-macos/lib/python3.7/site-packages/PyInstaller/building/osx.py")
 shutil.copyfile("src/build/fix/(pydub_fix)_audio_segment.py", "env-macos/lib/python3.7/site-packages/pydub/audio_segment.py")
